1ead9202f5b80fda112f20cc72c343c40037772f
DucTran2511 <122875189+DucTran2511@users.noreply.github.com>
2024-05-17 17:13:51 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -25,17 +25,14 @@ def main():
                                                                read_from_stub=True,
                                                                stub_path='D:\Code\Python\Data-Mining/final project/tracker_stubs/ball_detections.pkl')
     # Detect court
-    # court_tracker = CourtDetector('D:\Code\Python\Data-Mining/final project\model\corner_detector_model/best.pt')
-    # court_detections = court_tracker.detect_frames(frames=video_frames,
-    #                                                read_from_stub=True,
-    #                                                stub_path='D:\Code\Python\Data-Mining/final project/tracker_stubs/court_detection.pkl')
+    court_tracker = CourtDetector('D:\Code\Python\Data-Mining/final project\model\corner_detector_model/best.pt')
+    court_detections = court_tracker.crop_image(video_frames[0])
     # Detect corner
-    corner_detector = CornerDetector()
-    centers = corner_detector.convert_coordiante_size(video_frames[0],
-                                                      img_path='D:\Code\Python\Data-Mining/final project\corner_badminton_court_detector\sample_image\hehe.png',
-                                                      target_size=(640, 640))
+    corner_detector = CornerDetector(court_detections)
+    # Key points
+    key_points = corner_detector.convert_coordiante_size(court_tracker.get_bbox())
     # Track player
-    player_detections = player_tracker.player_tracking(centers, player_detections)
+    player_detections = player_tracker.player_tracking(key_points, player_detections)
     # Mini Court
     mini_court = MiniCourt(video_frames[0])
     # Draw player bounding boxes
@@ -44,8 +41,7 @@ def main():
     output_video_frames = shuttlecock_tracker.draw_bboxes(output_video_frames, shuttlecock_detections)
     # Draw corner of badminton court
     output_video_frames = corner_detector.draw_key_points_on_videos(output_video_frames,
-                                                                    sample_path='D:\Code\Python\Data-Mining/final project\corner_badminton_court_detector\sample_image\hehe.png',
-                                                                    target_size=(640, 640))
+                                                                    court_tracker.get_bbox())
     # Draw mini court
     output_video_frames = mini_court.draw_mini_court(video_frames)
     # Save video
