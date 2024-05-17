9b19d6f3d67ec498574795b996b865415caad826
DucTran2511 <122875189+DucTran2511@users.noreply.github.com>
2024-05-17 17:13:34 +0700

    Add files via upload

--- corner_badminton_court_detector/corner_badminton_court_detector.py
+++ corner_badminton_court_detector/corner_badminton_court_detector.py
@@ -4,18 +4,18 @@ from PIL import Image
 
 
 class CornerDetector:
+    def __init__(self, img_croped):
+        self.input = cv2.imread(img_croped)
 
-    @staticmethod
-    def binary_img(image_path, target_size):
-        raw_img = cv2.imread(image_path)
-        raw_img = cv2.resize(raw_img, target_size)
+    def binary_img(self):
+        raw_img = cv2.resize(self.input, (640, 640))
         gray_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
         ret, binary_img = cv2.threshold(gray_img, 190, 255, cv2.THRESH_BINARY)
         return gray_img, binary_img
 
-    def detect_edges(self, image_path, target_size):
+    def detect_edges(self):
         # Canny Edge Detection
-        gray_img, binary_img = self.binary_img(image_path, target_size)
+        gray_img, binary_img = self.binary_img()
         v = np.median(gray_img)
         sigma = 0.33
         lower = int(max(0, (1.0 - sigma) * v))
@@ -23,9 +23,9 @@ class CornerDetector:
         edges = cv2.Canny(binary_img, lower, upper, apertureSize=7)
         return edges
 
-    def segment_lines(self, image_path, target_size, deltaX=280, deltaY=0.5):
+    def segment_lines(self, deltaX=280, deltaY=0.5):
         # Hough Transform
-        edges = self.detect_edges(image_path, target_size)
+        edges = self.detect_edges()
         linesP = cv2.HoughLinesP(edges, 1, np.pi / 90, 90, None, 10, 250)
         h_lines = []
         v_lines = []
@@ -39,16 +39,23 @@ class CornerDetector:
         return h_lines, v_lines
 
     @staticmethod
-    def filterHorizontalLines(h_lines):
+    def filterHorizontalLines(h_lines, epsilon=20):
+        global upper_line, lower_line
         h_results = []
+        min_y = float('inf')
+        max_y = float('-inf')
         for segment in h_lines:
             for x1, y1, x2, y2 in segment:
-                if (y1 or y2) > 320:
-                    h_results.append(segment)
-                if (y1 or y2) < 320:
-                    segment[0][1] += 20
-                    segment[0][3] += 20
-                    h_results.append(segment)
+                if y1 < min_y:
+                    min_y = y1
+                    upper_line = segment
+                if y1 > max_y:
+                    max_y = y1
+                    lower_line = segment
+        upper_line[0][1] += epsilon
+        upper_line[0][3] += epsilon
+        h_results.append(upper_line)
+        h_results.append(lower_line)
         return h_results
 
     @staticmethod
@@ -70,10 +77,10 @@ class CornerDetector:
         _, _, centers = cv2.kmeans(points, nclusters, None, criteria, 10, cv2.KMEANS_PP_CENTERS)
         return centers
 
-    def find_intersection_points(self, img_path, target_size):
+    def find_intersection_points(self):
         Px = []
         Py = []
-        h_lines, v_lines = self.segment_lines(img_path, target_size)
+        h_lines, v_lines = self.segment_lines()
         filtered_h_lines = self.filterHorizontalLines(h_lines)
         for h_line in filtered_h_lines:
             for v_line in v_lines:
@@ -82,32 +89,32 @@ class CornerDetector:
                 Py.append(py)
         return Px, Py
 
-    def find_key_point_on_court(self, img_path, target_size):
-        Px, Py = self.find_intersection_points(img_path, target_size)
+    def find_key_point_on_court(self):
+        Px, Py = self.find_intersection_points()
         P = np.float32(np.column_stack((Px, Py)))
         nclusters = 10
         centers = self.cluster_points(P, nclusters)
         return centers
 
-    def convert_coordiante_size(self, dis_img_path, img_path, target_size):
-        centers = self.find_key_point_on_court(img_path, target_size)
-        image = Image.fromarray(dis_img_path)
-        dis_weight, dis_height = image.size
-        w, h = target_size
-        for i in range(len(centers) - 1):
-            centers[i][0], centers[i][1] = centers[i][0] * (dis_weight / w), centers[i][1] * (dis_height / w)
+    def convert_coordiante_size(self, bbox):
+        w, h = self.input.shape[0], self.input.shape[1]
+        centers = self.find_key_point_on_court()
+        for center in centers:
+            center[0] = (center[0] * (h / 640))
+            center[1] = (center[1] * (w / 640))
+        centers = centers + np.array([bbox[0][0],  bbox[0][1]], dtype=np.float32)
         return centers
 
-    def draw_points(self, centers, dis_img_path):
+    def draw_points(self, centers, frame):
         for i in range(len(centers) - 1):
             x, y = centers[i][0], centers[i][1]
-            cv2.putText(dis_img_path, str(i), (int(x), int(y) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            cv2.circle(dis_img_path, (int(x), int(y)), 5, (0, 0, 255), -1)
-        return dis_img_path
+            cv2.putText(frame, str(i), (int(x), int(y) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.circle(frame, (int(x), int(y)), 5, (0, 0, 255), -1)
+        return frame
 
-    def draw_key_points_on_videos(self, video_frames, sample_path, target_size=(640, 640)):
+    def draw_key_points_on_videos(self, video_frames, bbox):
         output_video_frames = []
-        centers = self.convert_coordiante_size(video_frames[0], sample_path, target_size)
+        centers = self.convert_coordiante_size(bbox)
         for frame in video_frames:
             frame = self.draw_points(centers, frame)
             output_video_frames.append(frame)
